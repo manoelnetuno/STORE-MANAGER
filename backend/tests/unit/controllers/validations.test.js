17a027f6fe5fc1cfb07a02a942e1c95547fed26e
Felipe Netuno <FELIPEMANOEL916@GMAIL.COM>
2024-08-13 21:23:13 -0300

    initial commit

--- backend/tests/unit/controllers/validations.test.js
+++ backend/tests/unit/controllers/validations.test.js
@@ -1,6 +1,7 @@
 const { expect } = require('chai');
 const sinon = require('sinon');
 const salemiddleware = require('../../../src/middlewares/validationsSales');
+const salemodel = require('../../../src/models/salesmodes');
 
 describe('realizando testes na validação das sales', function () {
   it('deve retornar erro 400 se não houver itens de venda', async function () {
@@ -15,7 +16,7 @@ describe('realizando testes na validação das sales', function () {
   });
 
   it('deve retornar erro 400 se algum item de venda não tiver productId', async function () {
-    const req = { body: [{ quantity: 1 }] };
+    const req = { body: [{ productId:1, quantity: 1 }, { quantity: 1}, { productId:-1, quantity: 2}] };
     const res = { status: sinon.stub().returns({ json: sinon.stub() }) };
     const next = sinon.stub();
 
@@ -46,7 +47,37 @@ describe('realizando testes na validação das sales', function () {
     expect(res.status).to.have.been.calledWith(422);
     expect(res.status().json).to.have.been.calledWith({ message: '"quantity" must be greater than or equal to 1' });
   });
+
+  it('deve retornar 404 quando o product id não existir', async function () {
+    req = {
+      body: [{ productId: 1000, quantity:5}],
+    };
+    res = {
+      status: sinon.stub().returnsThis(),
+      json: sinon.stub().returnsThis(),
+    };
+    next = sinon.stub();
+    await salemiddleware.validateProduct(req, res, next);
+    expect(res.status).to.have.been.calledWith(404);
+    expect(res.json).to.have.been.calledWith({
+      message: 'Product not found'
+    });
+  });
+
+  it('deve seguir ao proximo middleware depois do next', async function (){
+    sinon.stub(salemodel, 'getSalesId').resolves(true);
+    req = {
+      body: [{ productId: 1, quantity:5}],
+    };
+    res = {
+      status: sinon.stub().returnsThis(),
+      json: sinon.stub().returnsThis(),
+    };
+    next = sinon.stub();
+    await salemiddleware.validateProduct(req, res, next);
+    });
+  });
+
   afterEach(function () {
     sinon.restore();
   });
-});
--- backend/tests/unit/models/productmodel.test.js
+++ backend/tests/unit/models/productmodel.test.js
@@ -17,6 +17,29 @@ describe('Realizando testes - productModel', function () {
       name: validname,
     });
   });
+  it('PUT: deve atualizar um produto', async function () {
+    const validname = {
+      id: 4,
+      name:'joystick'}
+    sinon.stub(connection, 'execute').resolves([{ affectedRows: 1 }]);
+
+    const result = await productsModes.putUpdateProduct(validname.id, validname.name);
+
+    expect(result).to.be.deep.equal({
+      id: 4,
+      name: validname.name,
+    });
+  });
+  it('DELETE:deve deletar um produto', async function () {
+    const validname = {
+      id: 4,
+      name:'joystick'}
+    sinon.stub(connection, 'execute').resolves([{ affectedRows: 1 }]);
+
+    const result = await productsModes.deleteProduct(validname);
+
+    expect(result).to.be.deep.equal();
+  });
   afterEach(function () {
     sinon.restore();
   });
